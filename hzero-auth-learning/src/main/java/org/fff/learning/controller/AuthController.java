6709088a1e81f46d3453bdefd9c440d5b82f407a
Power-pot <fyj1316100@163.com>
2023-03-02 10:29:59 +0800

    no use db

--- hzero-auth-learning/src/main/java/org/fff/learning/controller/AuthController.java
+++ hzero-auth-learning/src/main/java/org/fff/learning/controller/AuthController.java
@@ -3,7 +3,6 @@ package org.fff.learning.controller;
 import io.choerodon.core.iam.ResourceLevel;
 import io.choerodon.swagger.annotation.Permission;
 import org.fff.learning.domain.User;
-import org.fff.learning.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +11,9 @@ import javax.annotation.Resource;
 @RestController
 public class AuthController {
 
-    @Resource
-    private UserService userService;
-
     @Permission(level = ResourceLevel.SITE, permissionPublic = true)
     @GetMapping("/hello")
     public String hello() {
-        User user = userService.getUserByUsername("Eugene");
-        return user.getUsername();
+        return "hello";
     }
 }
--- hzero-auth-learning/src/main/java/org/fff/learning/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package org.fff.learning.service;
-
-import org.fff.learning.domain.User;
-
-public interface UserService {
-    User getUserByUsername(String username);
-}
